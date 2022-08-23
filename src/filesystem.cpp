915e9f0a92d19b0c0ea1d17a1bdfc5e2da4e31a3
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-08-23 11:30:21 +0200

    Rework filesystem.hpp
    Add get_executable_path function to filesystem.hpp
    Add filesystem.cpp for implementation
    Remove old garbage filesystem.hpp

---
+++ src/filesystem.cpp
@@ -0,0 +1,35 @@
+
+#include "filesystem.hpp"
+
+/*
+#ifdef _WIN32
+#include <windows.h>
+#else
+*/
+#include <unistd.h>
+// #endif
+
+long get_executable_path(char *out)
+{
+/*
+#ifdef _WIN32
+    wchar_t path[MAX_PATH] = {0};
+    GetModuleFileNameW(NULL, path, MAX_PATH);
+    return path;
+#else
+*/
+    return readlink("/proc/self/exe", out, PATH_MAX);
+// #endif
+}
+
+std::string get_executable_path()
+{
+    char pth[PATH_MAX] = {0};
+
+    long len = get_executable_path(pth); 
+
+    if (len < 0)
+        return "";
+
+    return std::string(pth);
+}
