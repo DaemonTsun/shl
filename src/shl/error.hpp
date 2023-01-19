3ae4c4b2a77b7d31a11c680a8079a0ba6db7449d
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-01-19 10:29:43 +0100

    Add throw_error macro for line information

--- src/shl/error.hpp
+++ src/shl/error.hpp
@@ -3,11 +3,14 @@
 
 struct error
 {
+    const char *what;
+
     error();
     error(const char *what);
     error(const char *fmt, ...);
-
-    const char *what;
 };
 
 const char *format_error(const char *format, ...);
+
+#define throw_error(FMT, ...)\
+    throw error(__FILE__ " " #__LINE__ ": " FMT __VA_OPT__(,) __VA_ARGS__)
