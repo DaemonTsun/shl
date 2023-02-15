417fbce6b4b947d74c9b7ba917f9728b86d5a3be
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-02-15 19:00:36 +0100

    Add murmur2 to hash.cpp
    Remove <string_view> dependency from hash.cpp

--- src/shl/hash.cpp
+++ src/shl/hash.cpp
@@ -1,9 +1,50 @@
 
-#include <string_view>
-
 #include "shl/hash.hpp"
 
-hash_t hash_data(const char *data, u64 size)
+hash_t hash_data(const void *data, u64 size)
 {
-    return std::hash<std::string_view>()(std::string_view(data, size));
+    return hash_data(data, size, DEFAULT_MURMUR2_SEED);
 }
+
+hash_t hash_data(const void *_data, u64 size, u64 seed)
+{
+    const u64 m = 0xc6a4a7935bd1e995ull;
+    const int r = 47;
+
+    u64 h = seed ^ (size * m);
+
+    const u64 *data = (const u64 *)_data;
+    const u64 *end = data + (size / sizeof(u64));
+
+    while(data != end)
+    {
+        u64 k = *data++;
+
+        k *= m; 
+        k ^= k >> r; 
+        k *= m; 
+
+        h ^= k;
+        h *= m; 
+    }
+
+    const unsigned char *data2 = (const unsigned char *)data;
+
+    switch(size & 7)
+    {
+        case 7: h ^= static_cast<u64>(data2[6]) << 48;
+        case 6: h ^= static_cast<u64>(data2[5]) << 40;
+        case 5: h ^= static_cast<u64>(data2[4]) << 32;
+        case 4: h ^= static_cast<u64>(data2[3]) << 24;
+        case 3: h ^= static_cast<u64>(data2[2]) << 16;
+        case 2: h ^= static_cast<u64>(data2[1]) << 8;
+        case 1: h ^= static_cast<u64>(data2[0]);
+                h *= m;
+    };
+
+    h ^= h >> r;
+    h *= m;
+    h ^= h >> r;
+
+    return h;
+} 
--- src/shl/hash.hpp
+++ src/shl/hash.hpp
@@ -8,16 +8,21 @@
  * the hash_data(data, size) function maybe used to calculate the hash of arbitrary data.
  * hash_raw(T) is the same as hash_data, but using a type T and its size instead of
  * a pointer and size.
+ *
+ * default hashing algorithm used in hash_data is murmur2.
  */
 
 #include "shl/number_types.hpp"
 
 typedef u64 hash_t;
 
-hash_t hash_data(const char *data, u64 size);
+#define DEFAULT_MURMUR2_SEED 0xc70f6907ul
+
+hash_t hash_data(const void *data, u64 size);
+hash_t hash_data(const void *data, u64 size, u64 seed);
 
 template<typename T>
 hash_t hash_raw(const T *t)
 {
-    return hash_data(reinterpret_cast<const char *>(t), sizeof(T));
+    return hash_data(t, sizeof(T));
 }
