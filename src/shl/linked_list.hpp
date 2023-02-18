c701a4edcb24c9735b723f333b938bb2b0997358
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-02-18 17:43:17 +0100

    Add more linked_list functions
    Add nth_node, add_elements functions
    Add more linked lists tests

--- src/shl/linked_list.hpp
+++ src/shl/linked_list.hpp
@@ -48,34 +48,93 @@ void init(linked_list<T> *list, u64 n_elements)
 {
     assert(list != nullptr);
 
+    init(list);
+
     if (n_elements == 0)
-    {
-        init(list);
         return;
+
+    add_elements(list, n_elements);
+}
+
+template<typename T>
+list_node<T> *nth_node(linked_list<T> *list, u64 n)
+{
+    assert(list != nullptr);
+
+    if (n >= list->size)
+        return nullptr;
+
+    list_node<T> *ret = nullptr;
+    u64 half = list->size / 2;
+
+    if (n < half)
+    {
+        ret = list->first;
+        while (n > 0 && ret != nullptr)
+        {
+            ret = ret->next;
+            n--;
+        }
+    }
+    else
+    {
+        ret = list->last;
+        while (n < list->size - 1 && ret != nullptr)
+        {
+            ret = ret->previous;
+            n++;
+        }
     }
 
-    list->size = n_elements;
+    return ret;
+}
 
-    list->first = allocate_memory<list_node<T>>();
-    list->first->next = nullptr;
-    list->first->previous = nullptr;
+template<typename T>
+list_node<T> *add_elements(linked_list<T> *list, u64 n_elements)
+{
+    assert(list != nullptr);
 
-    list_node<T> *n = list->first;
+    if (n_elements == 0)
+        return nullptr;
 
-    for (u64 i = 1; i < n_elements; ++i)
+    list_node<T> *ret = nullptr;
+    u64 new_elements_to_add = n_elements;
+
+    if (list->last == nullptr)
+    {
+        assert(list->first == nullptr);
+
+        list->first = allocate_memory<list_node<T>>();
+        list->first->next = nullptr;
+        list->first->previous = nullptr;
+        list->last = list->first;
+
+        ret = list->last;
+        new_elements_to_add--;
+    }
+    
+    list_node<T> *n = list->last;
+
+    for (u64 i = 0; i < new_elements_to_add; ++i)
     {
         list_node<T> *tmp = allocate_memory<list_node<T>>();
         tmp->previous = n;
         tmp->next = nullptr;
         n->next = tmp;
         n = tmp;
+
+        if (ret == nullptr)
+            ret = tmp;
     }
 
     list->last = n;
+    list->size = list->size + n_elements;
+
+    return ret;
 }
 
 template<typename T>
-void free(linked_list<T> *list)
+void clear(linked_list<T> *list)
 {
     assert(list != nullptr);
 
@@ -91,4 +150,10 @@ void free(linked_list<T> *list)
     list->first = nullptr;
     list->last = nullptr;
     list->size = 0;
+}
+
+template<typename T>
+void free(linked_list<T> *list)
+{
+    clear(list);
 }
--- tests/linked_list_tests.cpp
+++ tests/linked_list_tests.cpp
@@ -51,4 +51,144 @@ define_test(init_initializes_array3)
     free(&list);
 }
 
+define_test(nth_node_gets_the_nth_node)
+{
+    linked_list<int> list;
+
+    init(&list, 5);
+
+    assert_equal(list.size, 5);
+
+    list_node<int> *node = nth_node(&list, 0);
+    assert_not_equal(node, nullptr);
+    assert_equal(node, list.first);
+    assert_equal(node, list.last->previous->previous->previous->previous);
+
+    node = nth_node(&list, 1);
+    assert_not_equal(node, nullptr);
+    assert_equal(node, list.first->next);
+    assert_equal(node, list.last->previous->previous->previous);
+
+    node = nth_node(&list, 2);
+    assert_not_equal(node, nullptr);
+    assert_equal(node, list.first->next->next);
+    assert_equal(node, list.last->previous->previous);
+
+    node = nth_node(&list, 3);
+    assert_not_equal(node, nullptr);
+    assert_equal(node, list.first->next->next->next);
+    assert_equal(node, list.last->previous);
+
+    node = nth_node(&list, 4);
+    assert_not_equal(node, nullptr);
+    assert_equal(node, list.first->next->next->next->next);
+    assert_equal(node, list.last);
+
+    node = nth_node(&list, 5);
+    assert_equal(node, nullptr);
+
+    free(&list);
+}
+
+define_test(add_elements_does_nothing_when_adding_no_elements)
+{
+    linked_list<int> list;
+
+    init(&list, 5);
+
+    assert_not_equal(list.first, nullptr);
+    assert_not_equal(list.last, nullptr);
+    assert_not_equal(list.first->next, nullptr);
+    assert_not_equal(list.last->previous, nullptr);
+    assert_equal(list.first->previous, nullptr);
+    assert_equal(list.last->next, nullptr);
+    assert_equal(list.size, 5);
+
+    list_node<int> *node = add_elements(&list, 0);
+
+    assert_equal(node, nullptr);
+    assert_equal(list.last->next, nullptr);
+    assert_equal(list.size, 5);
+
+    free(&list);
+}
+
+define_test(add_elements_adds_elements)
+{
+    linked_list<int> list;
+
+    init(&list, 5);
+
+    assert_not_equal(list.first, nullptr);
+    assert_not_equal(list.last, nullptr);
+    assert_not_equal(list.first->next, nullptr);
+    assert_not_equal(list.last->previous, nullptr);
+    assert_equal(list.first->previous, nullptr);
+    assert_equal(list.last->next, nullptr);
+    assert_equal(list.size, 5);
+
+    list_node<int> *previous_last = list.last;
+
+    list_node<int> *node = add_elements(&list, 1);
+
+    assert_not_equal(node, nullptr);
+    assert_equal(node, previous_last->next);
+    assert_equal(node, list.last);
+    assert_equal(list.size, 6);
+
+    free(&list);
+}
+
+define_test(add_elements_adds_elements2)
+{
+    linked_list<int> list;
+
+    init(&list, 5);
+
+    assert_not_equal(list.first, nullptr);
+    assert_not_equal(list.last, nullptr);
+    assert_not_equal(list.first->next, nullptr);
+    assert_not_equal(list.last->previous, nullptr);
+    assert_equal(list.first->previous, nullptr);
+    assert_equal(list.last->next, nullptr);
+    assert_equal(list.size, 5);
+
+    list_node<int> *previous_last = list.last;
+
+    list_node<int> *node = add_elements(&list, 3);
+
+    assert_not_equal(node, nullptr);
+    assert_equal(node, previous_last->next);
+    assert_equal(node, list.last->previous->previous);
+    assert_equal(list.size, 8);
+
+    free(&list);
+}
+
+define_test(add_elements_adds_elements_to_empty_list)
+{
+    linked_list<int> list;
+
+    init(&list);
+
+    assert_equal(list.first, nullptr);
+    assert_equal(list.last, nullptr);
+    assert_equal(list.size, 0);
+
+    list_node<int> *node = add_elements(&list, 5);
+
+    assert_not_equal(list.first, nullptr);
+    assert_not_equal(list.last, nullptr);
+    assert_not_equal(list.first->next, nullptr);
+    assert_not_equal(list.last->previous, nullptr);
+    assert_equal(list.first->previous, nullptr);
+    assert_equal(list.last->next, nullptr);
+    assert_equal(list.size, 5);
+
+    assert_not_equal(node, nullptr);
+    assert_equal(node, list.first);
+
+    free(&list);
+}
+
 define_default_test_main();
