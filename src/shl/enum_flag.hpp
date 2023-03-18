456b4322ac15a6e0f0eb74582da30ad4f6f7e003
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-03-18 14:51:33 +0100

    Fix enum_flag.hpp

--- src/shl/enum_flag.hpp
+++ src/shl/enum_flag.hpp
@@ -16,37 +16,37 @@
 #define JOIN(X, Y) X##Y
 
 #define DEFINE_ENUM_VALUE(T)\
-    constexpr inline underlying_type<T>::type value(T a) { return static_cast<underlying_type<T>::type>(a); }
+    constexpr inline underlying_type(T) value(T a) { return static_cast<underlying_type(T)>(a); }
 
 #define DEFINE_ENUM_UNDERLYING_TYPE(T)\
-    typedef underlying_type<T>::type JOIN(T, _ut);\
+    typedef underlying_type(T) JOIN(T, _ut);\
     DEFINE_ENUM_VALUE(T)
 
 #define ENUM_CLASS_FLAG_OPS(T)\
     DEFINE_ENUM_UNDERLYING_TYPE(T)\
-    constexpr inline underlying_type<T>::type operator~ (T a)       { return (~static_cast<underlying_type<T>::type>(a)); }\
-    constexpr inline T operator| (T a, T b)  { return static_cast<T>(static_cast<underlying_type<T>::type>(a) | static_cast<underlying_type<T>::type>(b)); }\
-    constexpr inline underlying_type<T>::type operator| (T a, underlying_type<T>::type b)  { return static_cast<underlying_type<T>::type>(a) | b; }\
-    constexpr inline underlying_type<T>::type operator| (underlying_type<T>::type a, T b)  { return a | static_cast<underlying_type<T>::type>(b); }\
-    constexpr inline T operator& (T a, T b)  { return static_cast<T>(static_cast<underlying_type<T>::type>(a) & static_cast<underlying_type<T>::type>(b)); }\
-    constexpr inline underlying_type<T>::type operator& (T a, underlying_type<T>::type b)  { return static_cast<underlying_type<T>::type>(a) & b; }\
-    constexpr inline underlying_type<T>::type operator& (underlying_type<T>::type a, T b)  { return a & static_cast<underlying_type<T>::type>(b); }\
-    constexpr inline T operator^ (T a, T b)  { return static_cast<T>(static_cast<underlying_type<T>::type>(a) ^ static_cast<underlying_type<T>::type>(b)); }\
-    constexpr inline underlying_type<T>::type operator^ (T a, underlying_type<T>::type b)  { return static_cast<underlying_type<T>::type>(a) ^ b; }\
-    constexpr inline underlying_type<T>::type operator^ (underlying_type<T>::type a, T b)  { return a ^ static_cast<underlying_type<T>::type>(b); }\
-    constexpr inline T& operator|=(T& a, T b) { return a = static_cast<T>(static_cast<underlying_type<T>::type>(a) | static_cast<underlying_type<T>::type>(b)); }\
-    constexpr inline T& operator|=(T& a, underlying_type<T>::type b) { return a = static_cast<T>(static_cast<underlying_type<T>::type>(a) | b); }\
-    constexpr inline T& operator&=(T& a, T b) { return a = static_cast<T>(static_cast<underlying_type<T>::type>(a) & static_cast<underlying_type<T>::type>(b)); }\
-    constexpr inline T& operator&=(T& a, underlying_type<T>::type b) { return a = static_cast<T>(static_cast<underlying_type<T>::type>(a) & b); }\
-    constexpr inline T& operator^=(T& a, T b) { return a = static_cast<T>(static_cast<underlying_type<T>::type>(a) ^ static_cast<underlying_type<T>::type>(b)); }\
-    constexpr inline T& operator^=(T& a, underlying_type<T>::type b) { return a = static_cast<T>(static_cast<underlying_type<T>::type>(a) ^ b); }\
+    constexpr inline underlying_type(T) operator~ (T a)       { return (~static_cast<underlying_type(T)>(a)); }\
+    constexpr inline T operator| (T a, T b)  { return static_cast<T>(static_cast<underlying_type(T)>(a) | static_cast<underlying_type(T)>(b)); }\
+    constexpr inline underlying_type(T) operator| (T a, underlying_type(T) b)  { return static_cast<underlying_type(T)>(a) | b; }\
+    constexpr inline underlying_type(T) operator| (underlying_type(T) a, T b)  { return a | static_cast<underlying_type(T)>(b); }\
+    constexpr inline T operator& (T a, T b)  { return static_cast<T>(static_cast<underlying_type(T)>(a) & static_cast<underlying_type(T)>(b)); }\
+    constexpr inline underlying_type(T) operator& (T a, underlying_type(T) b)  { return static_cast<underlying_type(T)>(a) & b; }\
+    constexpr inline underlying_type(T) operator& (underlying_type(T) a, T b)  { return a & static_cast<underlying_type(T)>(b); }\
+    constexpr inline T operator^ (T a, T b)  { return static_cast<T>(static_cast<underlying_type(T)>(a) ^ static_cast<underlying_type(T)>(b)); }\
+    constexpr inline underlying_type(T) operator^ (T a, underlying_type(T) b)  { return static_cast<underlying_type(T)>(a) ^ b; }\
+    constexpr inline underlying_type(T) operator^ (underlying_type(T) a, T b)  { return a ^ static_cast<underlying_type(T)>(b); }\
+    constexpr inline T& operator|=(T& a, T b) { return a = static_cast<T>(static_cast<underlying_type(T)>(a) | static_cast<underlying_type(T)>(b)); }\
+    constexpr inline T& operator|=(T& a, underlying_type(T) b) { return a = static_cast<T>(static_cast<underlying_type(T)>(a) | b); }\
+    constexpr inline T& operator&=(T& a, T b) { return a = static_cast<T>(static_cast<underlying_type(T)>(a) & static_cast<underlying_type(T)>(b)); }\
+    constexpr inline T& operator&=(T& a, underlying_type(T) b) { return a = static_cast<T>(static_cast<underlying_type(T)>(a) & b); }\
+    constexpr inline T& operator^=(T& a, T b) { return a = static_cast<T>(static_cast<underlying_type(T)>(a) ^ static_cast<underlying_type(T)>(b)); }\
+    constexpr inline T& operator^=(T& a, underlying_type(T) b) { return a = static_cast<T>(static_cast<underlying_type(T)>(a) ^ b); }\
     constexpr inline bool is_set(T a, T b) { return (a & b) == b; }\
-    constexpr inline bool is_set(T a, underlying_type<T>::type b) { return (a & b) == b; }\
-    constexpr inline bool is_set(underlying_type<T>::type a, T b) { return static_cast<T>(a & b) == b; }\
+    constexpr inline bool is_set(T a, underlying_type(T) b) { return (a & b) == b; }\
+    constexpr inline bool is_set(underlying_type(T) a, T b) { return static_cast<T>(a & b) == b; }\
     constexpr inline T& set(T& a, T b) { return a |= b; } \
-    constexpr inline T& set(T& a, underlying_type<T>::type b) { return a |= b; } \
+    constexpr inline T& set(T& a, underlying_type(T) b) { return a |= b; } \
     constexpr inline T& unset(T& a, T b) { return a &= ~b; } \
-    constexpr inline T& unset(T& a, underlying_type<T>::type b) { return a &= ~b; }
+    constexpr inline T& unset(T& a, underlying_type(T) b) { return a &= ~b; }
 
 #define enum_flag(T)\
     ENUM_CLASS_FLAG_OPS(T)
