537cf13a802a2d610b7a265bde26caa3bb5e0fff
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2023-12-27 19:45:38 +0100

    Update macros.hpp
    Add GET_MACRO4-6
    Add checks if macros are already defined or not...

--- src/shl/macros.hpp
+++ src/shl/macros.hpp
@@ -1,8 +1,31 @@
 
 #pragma once
 
+#ifndef JOIN
 #define JOIN(X, Y) X##Y
-#define JOIN3(X, Y, Z) X##Y##Z
+#endif
 
+#ifndef JOIN3
+#define JOIN3(X, Y, Z) X##Y##Z
+#endif
+
+#ifndef GET_MACRO2
 #define GET_MACRO2(_1, _2, _3, NAME, ...) NAME
+#endif
+
+#ifndef GET_MACRO3
 #define GET_MACRO3(_1, _2, _3, _4, NAME, ...) NAME
+#endif
+
+#ifndef GET_MACRO4
+#define GET_MACRO4(_1, _2, _3, _4, _5, NAME, ...) NAME
+#endif
+
+#ifndef GET_MACRO5
+#define GET_MACRO5(_1, _2, _3, _4, _5, _6, NAME, ...) NAME
+#endif
+
+#ifndef GET_MACRO6
+#define GET_MACRO6(_1, _2, _3, _4, _5, _6, _7, NAME, ...) NAME
+#endif
+
