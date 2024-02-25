d9fc54fb17e83cee168d4775f692b0e0ad1749cd
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2024-02-25 18:38:16 +0100

    Fix wide print?

--- src/shl/print.cpp
+++ src/shl/print.cpp
@@ -1,4 +1,5 @@
 
+#include <stdlib.h> // wcstombs
 #include "shl/print.hpp"
 
 s64 put(io_handle h, char c, error *err)
@@ -11,23 +12,29 @@ s64 put(io_handle h, wchar_t c, error *err)
     return io_write(h, (char*)&c, sizeof(wchar_t), err);
 }
 
-template<typename C>
-s64 _put_cs(io_handle h, const_string_base<C> s, error *err)
-{
-    return io_write(h, (const char*)s.c_str, s.size * sizeof(C), err);
-}
-
 s64 _put(io_handle h, const_string  s, error *err)
 {
-    return _put_cs(h, s, err);
+    return io_write(h, s.c_str, s.size, err);
 }
 
 s64 _put(io_handle h, const_wstring s, error *err)
 {
-    return _put_cs(h, s, err);
+    internal::tformat_buffer *buf = internal::_get_tformat_buffer_char();
+
+    char *start = buf->buffer.data + buf->offset;
+    u64 bytes_written = wcstombs(start, s.c_str, buf->buffer.size);
+
+    if (bytes_written == (u64)-1)
+        return -1;
+
+    buf->buffer.data[buf->offset + bytes_written] = '\0';
+    
+    internal::_buffer_advance<char>(buf, bytes_written);
+
+    return _put(h, const_string{start, bytes_written}, err);
 }
 
 s64 put(char    c, error *err)        { return put(stdout_handle(), c, err); }
 s64 put(wchar_t c, error *err)        { return put(stdout_handle(), c, err); }
-s64 _put(const_string  s, error *err) { return _put_cs(stdout_handle(), s, err); }
-s64 _put(const_wstring s, error *err) { return _put_cs(stdout_handle(), s, err); }
+s64 _put(const_string  s, error *err) { return _put(stdout_handle(), s, err); }
+s64 _put(const_wstring s, error *err) { return _put(stdout_handle(), s, err); }
--- tests/print_tests.cpp
+++ tests/print_tests.cpp
@@ -6,4 +6,6 @@ int main(int argc, char **argv)
     tprint("hello %\n", "world");
     tprint(stdout_handle(), "hello %\n", "world");
     tprint(stderr_handle(), "hello %\n", "world");
+
+    tprint(L"wide hello %\n", L"world");
 }
