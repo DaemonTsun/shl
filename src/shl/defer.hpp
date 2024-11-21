f3e48e1a6ceedad7dc6456f150aa664a8951c012
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2024-11-21 18:04:41 +0100

    Add static_exec, thread_local_exec

--- src/shl/defer.hpp
+++ src/shl/defer.hpp
@@ -3,7 +3,7 @@
 
 /* defer.hpp by https://stackoverflow.com/a/42060129
 
-allows the use of defer { ... }; blocks in code.
+Allows the use of defer { ... }; blocks in code.
 
 example:
 
@@ -13,6 +13,20 @@ defer { free(&a); };
 tprint("% world!\n", &a);
 
 // no need to free here since the free is deferred
+
+
+static_defer { Code; };
+    Defers execution of Code to the end of the lifetime of a static object.
+
+thread_local_defer { Code; }
+    Defers execution of Code to the end of the lifetime of a thread_local object.
+
+static_exec { Code; };
+    Calls Code once as a static execution, as if Code were the constructor of a static object.
+
+thread_local_exec { Code; };
+    Calls Code once as a thread_local execution, as if Code were the constructor of a thread_local object.
+
 */
 
 #if defined(_WIN32) || defined(_WIN64)
@@ -44,8 +58,40 @@ _deferrer<F> operator*(_defer, F f)
 }
 
 #ifndef defer
-#define _DEFER2(X) zz_defer##X
-#define _DEFER(X) _DEFER2(X)
-#define defer auto _DEFER(__LINE__) = _defer{} * [&]()
-#define static_defer static auto _DEFER(__LINE__) = _defer{} * []()
+#  define _DEFER2(X) zz_defer##X
+#  define _DEFER(X) _DEFER2(X)
+#  define defer auto _DEFER(__LINE__) = _defer{} * [&]()
+#  define static_defer static auto _DEFER(__LINE__) = _defer{} * []()
+#  define thread_local_defer thread_local auto _DEFER(__LINE__) = _defer{} * []()
+#endif
+
+// exec
+#if defined(_WIN32) || defined(_WIN64)
+#pragma warning(push)
+#pragma warning(disable : 4623) // implicitly deleted constructor
+#pragma warning(disable : 4626) // implicitly deleted assignment operator
+#endif
+struct _d_execute {};
+template <class F> struct _d_executer
+{
+    _d_executer(F f)
+    {
+        f();
+    }
+};
+#if defined(_WIN32) || defined(_WIN64)
+#pragma warning(pop) 
+#endif
+
+template <class F>
+_d_executer<F> operator*(_d_execute, F f)
+{
+    return _d_executer(f);
+}
+
+#ifndef static_exec
+#  define _EXEC2(X) zz_exec##X
+#  define _EXEC(X) _EXEC2(X)
+#  define static_exec static auto _EXEC(__LINE__) = _d_execute{} * []()
+#  define thread_local_exec thread_local auto _EXEC(__LINE__) = _d_execute{} * []()
 #endif
--- tests/defer_tests.cpp
+++ tests/defer_tests.cpp
@@ -1,6 +1,10 @@
 
 #include <t1/t1.hpp>
 
+#undef defer
+
+#include "shl/memory.hpp"
+#include "shl/print.hpp"
 #include "shl/defer.hpp"
 
 define_test(file_stream_init_initializes)
@@ -23,6 +27,36 @@ define_test(file_stream_init_initializes)
     assert_equal(x, 2);
 
     // x would be 3 after this block
+}
+
+void some_func()
+{
+    static int *x = nullptr;
+
+    // x gets allocated and assigned exactly once, no matter how many times the
+    // function is called.
+    static_exec {
+        tprint("some_func: static_exec called\n");
+        x = alloc<int>();
+        *x = 13;
+    };
+
+    // x gets freed exactly once.
+    static_defer {
+        tprint("some_func: static_defer called\n");
+        dealloc(x);
+        x = nullptr;
+    };
+
+    assert_not_equal(x, nullptr);
+    assert_equal(*x, 13);
+}
+
+define_test(static_defer_and_static_exec_test)
+{
+    some_func();
+    some_func();
+    some_func();
 }
 
 define_default_test_main();
